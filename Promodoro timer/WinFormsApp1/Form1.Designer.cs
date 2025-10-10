40e235437d747468c5194686335f695804072d09
Mimi <michelle.thomas157@gmail.com>
2025-10-10 11:17:52 +0200

    commit
    Just played around. silly mee lol

--- Promodoro timer/WinFormsApp1/Form1.Designer.cs
+++ Promodoro timer/WinFormsApp1/Form1.Designer.cs
@@ -28,45 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TimerBox));
             BtnStart = new Button();
-            lbl1 = new Label();
+            lbl = new Label();
             btnreset = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lbl2 = new Label();
             SuspendLayout();
             // 
             // BtnStart
             // 
             BtnStart.BackColor = Color.RosyBrown;
+            BtnStart.BackgroundImage = Timer.Properties.Resources._20250306_215610;
             BtnStart.Font = new Font("MV Boli", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             BtnStart.Image = Timer.Properties.Resources._20250306_215610;
-            BtnStart.Location = new Point(12, 161);
+            BtnStart.Location = new Point(9, 144);
             BtnStart.Name = "BtnStart";
             BtnStart.Size = new Size(94, 54);
             BtnStart.TabIndex = 2;
             BtnStart.Text = "Start";
             BtnStart.UseVisualStyleBackColor = false;
+            BtnStart.Click += BtnStart_Click;
             // 
-            // lbl1
+            // lbl
             // 
-            lbl1.Font = new Font("MV Boli", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl1.Image = Timer.Properties.Resources._20250306_215610;
-            lbl1.Location = new Point(12, 24);
-            lbl1.Name = "lbl1";
-            lbl1.Size = new Size(196, 113);
-            lbl1.TabIndex = 3;
-            lbl1.Text = "Time ";
-            lbl1.TextAlign = ContentAlignment.TopCenter;
+            lbl.BackColor = Color.RosyBrown;
+            lbl.Font = new Font("MV Boli", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl.Image = Timer.Properties.Resources._20250306_215610;
+            lbl.Location = new Point(9, 11);
+            lbl.Name = "lbl";
+            lbl.Size = new Size(196, 87);
+            lbl.TabIndex = 3;
+            lbl.Text = "00:00";
+            lbl.TextAlign = ContentAlignment.TopCenter;
+            lbl.Click += lbl_Click;
             // 
             // btnreset
             // 
+            btnreset.BackColor = Color.RosyBrown;
+            btnreset.BackgroundImage = Timer.Properties.Resources._20250306_215610;
             btnreset.Font = new Font("MV Boli", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnreset.Image = Timer.Properties.Resources._20250306_215610;
-            btnreset.Location = new Point(114, 161);
+            btnreset.Location = new Point(111, 144);
             btnreset.Name = "btnreset";
             btnreset.Size = new Size(94, 54);
             btnreset.TabIndex = 4;
             btnreset.Text = "Reset";
-            btnreset.UseVisualStyleBackColor = true;
+            btnreset.UseVisualStyleBackColor = false;
+            btnreset.Click += btnreset_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // lbl2
+            // 
+            lbl2.AutoSize = true;
+            lbl2.Font = new Font("MV Boli", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl2.Image = Timer.Properties.Resources._20250306_215610;
+            lbl2.Location = new Point(87, 112);
+            lbl2.Name = "lbl2";
+            lbl2.Size = new Size(48, 20);
+            lbl2.TabIndex = 5;
+            lbl2.Text = "0,0h";
+            lbl2.Click += lbl2_Click;
             // 
             // TimerBox
             // 
@@ -74,19 +101,23 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(255, 192, 255);
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(225, 219);
+            ClientSize = new Size(213, 210);
+            Controls.Add(lbl2);
             Controls.Add(btnreset);
-            Controls.Add(lbl1);
+            Controls.Add(lbl);
             Controls.Add(BtnStart);
             Margin = new Padding(2);
             Name = "TimerBox";
             Text = "Promodoro Timer";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Button BtnStart;
-        private Label lbl1;
+        private Label lbl;
         private Button btnreset;
+        private System.Windows.Forms.Timer timer1;
+        private Label lbl2;
     }
 }
--- Promodoro timer/WinFormsApp1/Form1.cs
+++ Promodoro timer/WinFormsApp1/Form1.cs
@@ -1,12 +1,50 @@
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
 namespace WinFormsApp1
 {
     public partial class TimerBox : Form
     {
+        public TimeSpan timeleft;
+        public TimeSpan breaktime;
+
         public TimerBox()
         {
             InitializeComponent();
+            
         }
 
-       
+        private void lbl_Click(object sender, EventArgs e)
+        {
+            // Unnötige Methode für dieses Projekt 
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void lbl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+             TimeSpan timeleft = new TimeSpan(0, 25, 0);
+             TimerBox.Text = TimerBox.ToString(@"mm\:ss");
+             timeleft = timeleft.Add(TimeSpan.FromSeconds(-1));
+
+
+
+
+        }
     }
 }
